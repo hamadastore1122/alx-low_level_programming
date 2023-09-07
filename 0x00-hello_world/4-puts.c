648cfd24e0b53f598f6e320d8d0bb1d8774a0fd7
hamadastore1122 <shehabstore22@gmail.com>
2023-09-08 01:55:17 +0300

    hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#!/bin/bash
+#include<stdio.h>
+/**
+ * man-Entry point
+ * 
+ * Return:Always 0(success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
