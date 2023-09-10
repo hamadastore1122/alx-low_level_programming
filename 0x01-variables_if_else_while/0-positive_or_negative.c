9e2c15ba7b107bd82579e147380746224a1f9f4a
hamadastore1122 <shehabstore22@gmail.com.com>
2023-09-10 08:20:51 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -5,27 +5,28 @@
  * main - entry point
  * descreption 'check for numpers if it is positive  or negitive'
  * return always o
+ */
 
 /* betty style doc for function main goes there */
 int main(void)
 {
-		int n;
+                int n;
 
-			srand(time(0));
-				n = rand() - RAND_MAX / 2;
-					if (n>0)
-					{
+                        srand(time(0));
+                                n = rand() - RAND_MAX / 2;
+                                        if (n>0)
+                                        {
 
-							            printf("%d is positive\n", n);
-								        }
-					    else if (n == 0)
-						        {
-								        printf("%d is zero\n", n);
-									    }
-					        else
-							    {
-								            printf("%d is  negetive\n",n);
-									        }
-							return (0);
-							
+                                                                    printf("%d is positive\n", n);
+                                                                        }
+                                            else if (n == 0)
+                                                        {
+                                                                        printf("%d is zero\n", n);
+                                                                            }
+                                                else
+                                                            {
+                                                                            printf("%d is  negetive\n",n);
+                                                                                }
+                                                        return (0);
+
 }
