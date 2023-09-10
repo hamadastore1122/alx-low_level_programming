6fb6c39ded0ad97b9ed62dbcfb50a04762e2723e
hamadastore1122 <shehabstore22@gmail.com.com>
2023-09-10 15:20:18 -0700

    updattet

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,7 @@
-#!/bin/bash
-#include<stdio.h>
-/**
- * man - Enty Point 
- *
- * Return :Always0(Success)
- */
-int main(void)
-{
-	puts("\"programming is like building a multilingual puzzle");
-	return(0);
-}
+#include <stdio.h>
+int main() {
+   // printf() displays the string inside quotation
+   printf("\"Programming is like building a multilingual puzzle");
+   return (0);
 
+}
