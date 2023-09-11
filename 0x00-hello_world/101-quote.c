e08f542f859713864ab64b816850eede6e6b1867
hamadastore1122 <shehabstore22@gmail.com.com>
2023-09-10 17:15:10 -0700

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,12 @@
-#!/bin/bash
 #include<stdio.h>
-#include<unisted.h>
+#include<unistd.h>
 /**
  * main - Enty point
  *
- * *Return:Always 1 (success)
+ * *Return:Always 1(success)
  */
 int main(void)
 {
-	write(2,
-		"and that pice of art is useful\n"-Dora Korpar,2015-10-19/n",59);
+	write("and that piece of art is useful" - Dora Korpar, 2015-10-19");
 	return(1);
 }
