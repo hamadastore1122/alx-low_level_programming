d114b6b6d905eac38bd3e0d2cc38e804aa68691d
hamadastore1122 <shehabstore22@gmail.com>
2023-09-11 19:55:32 -0700

    updattet

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
+#include<time.h>
+#include<stdio.h>
 /**
  * main - Entry point
- *
+ * Descreption: 'git the return value of numbers'
  * Return Always 0 (success)
  */
 int main(void)
 {
 	printf("\"Programming is like a multilingual puzzle");
-	return(0);
+	return (0);
 }
