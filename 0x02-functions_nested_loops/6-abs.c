3da2f64329edbe94514a316903e163b5ec3f0668
hamadastore1122 <shehabstore22@gmail.com>
2023-09-12 20:40:20 -0700

    updatet

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _abs - computes the absloute value of an integer.
+ * @c: the numper to be value of number or zero
+ */
+int _abs(int c)
+{
+	if (c < 0)
+	{
+	int s;
+	s = c * -1;
+
+	return (s);
+	}
+	return (c);
+}	
