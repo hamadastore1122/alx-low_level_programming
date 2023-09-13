8ba9d274c50524945dca12144a34b26e721d72b0
hamadastore1122 <shehabstore22@gmail.com>
2023-09-13 05:11:56 -0700

    sign update

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,27 +1,27 @@
 #include "main.h"
 
 /**
- * print_sing - prints the sing of a number
- * @: the number to be checked
+ * print_sign - prints the sing of a number
+ * @n: the number to be checked
  * Return: 1 for positive num, -1 for negative num or zero for anything else
  */
 
 int print_sign(int n)
 
 {
-	if (n > 0)
-	{
-		_putchar(43);
-		return (1);
-	}
-	else if (n < 0)
-	{
-		_putchar(45);
-		return (-1);
-	}
-	else
-	{
-		_putchar(48);
-		return (0);
-	}
+if (n > 0)
+{
+_putchar(43);
+return (1);
+}
+else if (n < 0)
+{
+_putchar(45);
+return (-1);
+}
+else
+{
+_putchar(48);
+return (0);
+}
 }
