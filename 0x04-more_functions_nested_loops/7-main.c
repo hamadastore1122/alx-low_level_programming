aafb0df9f31ee62ef010a2586d8935bf837009e7
hamadastore1122 <shehabstore22@gmail.com>
2023-09-15 18:37:07 -0700

    7-diagonals is updattet

--- 0x04-more_functions_nested_loops/7-main.c
+++ 0x04-more_functions_nested_loops/7-main.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * main - check the code
  *
@@ -7,9 +6,10 @@
  */
 int main(void)
 {
-    print_diagonal(0);
-    print_diagonal(2);
-    print_diagonal(10);
-    print_diagonal(-4);
-    return (0);
+
+print_diagonal(0);
+print_diagonal(2);
+print_diagonal(10);
+print_diagonal(-4);
+return (0);
 }
