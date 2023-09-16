73e5a1265fcc5010834fa25f41b38bb2bc9b34a1
hamadastore1122 <shehabstore22@gmail.com>
2023-09-15 19:51:14 -0700

    updatet

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_line -straight -linr
+ * @n:no of times
+ * Return:always 0
+ */
+void print_line(int n)
+{
+	int i;
+for (i = 0; i < n; i++)
+{
+_putchar(' ');
+}
+_putchar('\n');
+}
