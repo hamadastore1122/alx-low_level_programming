22194ca93b0f716bf8da08618b5151f1b7ae7691
hamadastore1122 <shehabstore22@gmail.com>
2023-09-18 12:50:49 -0700

    puts is updatet

--- 0x05-pointers_arrays_strings/3-main.c
+++ 0x05-pointers_arrays_strings/3-main.c
@@ -7,9 +7,10 @@
  */
 int main(void)
 {
-    char *str;
 
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-    _puts(str);
-    return (0);
+char *str;
+
+str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+_puts(str);
+return (0);
 }
