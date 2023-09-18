3ba76a05890b832631bb5703b423230413d60a78
hamadastore1122 <shehabstore22@gmail.com>
2023-09-18 12:08:51 -0700

    update

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _strlen - checks the length of the strig
+ * @s:-string to be checked
+ * Return:the length of the string
+ */
+int _strlen(char *s)
+
+{
+int string_legth = 0;
+
+while (s[string_legth])
+string_legth;
+return (string_legth);
+}
