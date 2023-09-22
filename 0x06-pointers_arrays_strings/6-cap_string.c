d566becb7b2ad496d26e974af16864a362938f48
hamadastore1122 <shehabstore22@gmail.com>
2023-09-22 15:24:17 -0700

    101-print_number.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,37 +1,36 @@
 #include "main.h"
 /**
- * cap_string - function that capitalizws
- * all worlds of a string.
- * @s: pointer to string.
- * Return: pointer to s.
+ * cap_string - capitalizes most of the words in a string.
+ * @s: analized string.
+ *
+ * Return: String with all words capitalized.
  */
 char *cap_string(char *s)
+{
+	int i, j;
+	int a[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
-{
-int string_count:
-/* scan through string */
-string_count = 0;
-while (s[string_count] != '\0')
-
-{
-/* if next character after count is a chare , capitalise */
-if (s[0] >= 97 && s[0] <= 122)
-
-{
-s[0] = s[0] - 32;
-}
-if (s[string_count] == ' ' || s[string_count] == '\t' ||s[string_count] == '\n'
-|| s[string_count] == ',' || s[string_count] == ';' || s[strig_count] == '.'
-|| s[string-count] == '.' || s[string_count] == '!' || s[string_count] == '?'
-|| s[string_count] == ':' || s[string_count] == '(' || s[string_count] == ')'
-|| s[string_count] == '{' || s[string_coount] '}')
-{
-if (s[string_count + 1] >= 97 && s[string_count + 1] <= 122)
-{
-s[string_count + 1] >= 97 && s[string_count + 1] - 32;
-}
-}
-string_count++;
-}
-return (0);
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		{
+			if (i == 0)
+			{
+				*(s + i) = *(s + i) - 32;
+			}
+			else
+			{
+				for (j = 0; j <= 12; j++)
+				{
+					if (a[j] == *(s + i - 1))
+					{
+						*(s + i) = *(s + i) - 32;
+					}
+				}
+			}
+		}
+	i++;
+	}
+	return (s);
 }
