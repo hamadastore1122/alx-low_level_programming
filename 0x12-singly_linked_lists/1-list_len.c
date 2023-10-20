9d5c2b7740963ed2076d70f999543f19dd68ef8a
hamadastore1122 <shehabstore22@gmail.com>
2023-10-19 18:22:09 -0700

    linked list_t is updattet

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,17 +1,21 @@
 #include "lists.h"
+
 /**
- * list_len - returns then number of elements in a list.
- * @h: singly linked list .
- * Return: number of elements in the list.
- */
+ * list_len - finds the number of elements in a linked list
+ *@h: pointer to the list
+ *
+ *Return: the number of elements
+*/
+
 size_t list_len(const list_t *h)
-{
-	size_t nelem;
-	nelem = 0;
-	while (h != NULL)
 	{
-		h = h->next;
-		nelem++;
+		size_t elements = 0;
+
+		while (h)
+		{
+			elements++;
+			h = h->next;
+		}
+
+		return (elements);
 	}
-	return (nelem);
-}
--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,17 +1,20 @@
 #include "lists.h"
+
 /**
- * free_list - free a list
- * @head: A pointer to the list head
+ *free_list - Frees a list
+ *@head: A pointer to the list head
  *
  */
+
 void free_list(list_t *head)
 {
-list_t *temp;
-while (head)
-{
-tmp = head->next;
-free(head->str);
-free(head);
-head = tmp;
-}
+	list_t *tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head->str);
+		free(head);
+		head = tmp;
+	}
 }
