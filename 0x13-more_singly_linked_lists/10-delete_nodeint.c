ccf98a7d3f08680d250da671c453ee0cc58195b4
hamadastore1122 <shehabstore22@gmail.com>
2023-10-24 06:12:48 -0700

    update

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,20 +1,20 @@
 #include "lists.h"
 #include <stdlib.h>
+
 /**
  * delete_nodeint_at_index - function that deletes the node at index,
- * index if a listint_t linked list.
- * @head: pointer to pointer to the head if linked list.
- * @index:index of the node that should be deletet. Index starts at 0.
+ * index of a listint_t linked list.
+ * @head: pointer to pointer to the head of linked list.
+ * @index:index of the node that should be deleted. Index starts at 0.
  *
- * Return: 1 if it succeeded, -1 if it failed.
+ * Return:  1 if it succeeded, -1 if it failed.
  */
-int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	listint_t *temp_variable, *temp_variable2;
 	unsigned int i = 0;
-
-	/* if there is ni list there return NULL */
+	/* if there is no list then return NULL */
 	if (head == NULL || *head == NULL)
 		return (-1);
 	/* check for index 0 */
@@ -24,8 +24,10 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		*head = (*head)->next;
 		free(temp_variable);
 		return (1);
-		}
-	/* assign the temporary var;iable as the first node */
+	}
+	/* assign the temporary variable as the first node */
+	temp_variable = *head;
+	/* traverse the list till the index */
 	while (i != index - 1 && temp_variable->next != NULL)
 	{
 		temp_variable = temp_variable->next;
@@ -37,7 +39,6 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		temp_variable->next = temp_variable2->next;
 		free(temp_variable2);
 		return (1);
-	}
-	/* by here the code has failed so we return -1 */
+	} /* by here the code has failed so we return -1 */
 	return (-1);
 }
