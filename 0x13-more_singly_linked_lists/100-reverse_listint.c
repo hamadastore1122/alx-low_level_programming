8773d113589fd8da1567fd2f93be0b1698a49139
hamadastore1122 <shehabstore22@gmail.com>
2023-10-24 06:10:36 -0700

    updatet

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -5,18 +5,18 @@
  * reverse_listint - function that reverses a listint_t linked list.
  * @head: pointer to pointer to the head of linked list.
  *
- * YOu are not allowed to use more than 1 loop.
+ * You are not allowed to use more than 1 loop.
  * You are not allowed to use malloc, free or arrays.
- * You can only declare a maximum of tow variables in you function.
+ * You can only declare a maximum of two variables in your function.
  *
- * there should be no memory leaks.
+ * There should be no memory leaks.
  *
- * Return: the pointer to first node.
+ * Return:  the pointer to first node.
  */
 
 listint_t *reverse_listint(listint_t **head)
 {
-	/* variable to keep track of the current head */
+	/*variable to keep track of the current head */
 	listint_t *current = *head;
 
 	/* if we have no list or ptr to ptr to the head of the list */
@@ -24,7 +24,7 @@ listint_t *reverse_listint(listint_t **head)
 	{
 		return (NULL);
 	}
-	/*check if the next field of current is null */
+	/* check if the next field of current is null */
 	if (current->next == NULL)
 	{
 		/* make pointer to head as current and return it */
@@ -35,15 +35,18 @@ listint_t *reverse_listint(listint_t **head)
 	/* to first node */
 	else if (*head && (*head)->next)
 	{
-		/* declare a temp variable and assign it as first nide */
+		/* declare a temp variable and assign it as first node */
 		listint_t *temp_variable = *head;
 		/* access the next field */
 		*head = (*head)->next;
-		/*reverse it recursively */
+		/* reverse it recursively */
 		reverse_listint(head);
-		/* access the data at the memory location to which temp_variable->nexxt */
+		/* access the data at the memory location to which temp_variable->next */
+		/* points to and set it as current */
+		temp_variable->next->next = current;
+		/* access the next field of temp_variable and set it as NULL */
 		temp_variable->next = NULL;
 	}
-	/* return the pointer to the firest node */
+	/* return the pointer to the first node */
 	return (*head);
 }
